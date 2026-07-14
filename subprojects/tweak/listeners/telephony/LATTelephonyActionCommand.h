9a58bdccc7b923e6a0491d7e4dfd1d7c2a39ea54
Lessica <82flex@gmail.com>
2026-07-14 21:19:04 +0800

    Move nullability annotation below telephony action enum

--- subprojects/tweak/listeners/telephony/LATTelephonyActionCommand.h
+++ subprojects/tweak/listeners/telephony/LATTelephonyActionCommand.h
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 typedef NS_ENUM(NSUInteger, LATTelephonyActionKind) {
     LATTelephonyActionKindAnswerCall,
     LATTelephonyActionKindDisconnectCall,
 };
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface LATTelephonyActionCommand : NSObject
 
